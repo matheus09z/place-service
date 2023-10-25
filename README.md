b3aef9a559c884cae895be1700c4b5574178a8b4
matth <mattheew.oliveira@gmail.com>
2023-10-25 10:45:29 -0300

    Enviando Projeto

---
+++ README.md
@@ -0,0 +1,20 @@
+Place Service
+
+API para gerenciar lugares (CRUD) que faz parte desse desafio para pessoas desenvolvedoras backend que se candidatam para a ClickBus.
+
+Tecnologias
+Spring Boot
+Spring Webflux
+Spring Data + R2DBC
+SpringDoc OpenAPI 3
+Slugify
+Práticas adotadas
+SOLID
+Testes automatizados
+Consultas com filtros dinâmicos usando o Query By Example
+API reativa na web e na camada de banco
+Uso de DTOs para a API
+Injeção de Dependências
+Geração automática do Swagger com a OpenAPI 3
+Geração de slugs automática com o Slugify
+Auditoria sobre criação e atualização da entidade
---
+++ src/test/java/br/com/matheus/placeservice/PlaceServiceApplicationTests.java
@@ -0,0 +1,48 @@
+package br.com.matheus.placeservice;
+
+import br.com.matheus.placeservice.api.PlaceRequest;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class PlaceServiceApplicationTests {
+	@Autowired
+	WebTestClient webTestClient;
+
+	@Test
+	public void testCreatePlaceSucess() {
+		var name = "Valid Name";
+		var state = "Valid State";
+		var slug = "valid-name";
+
+		webTestClient
+				.post()
+				.uri("/places")
+				.bodyValue(
+						new PlaceRequest(name, state))
+				.exchange()
+				.expectStatus().isCreated()
+				.expectBody()
+				.jsonPath("name").isEqualTo(name)
+				.jsonPath("state").isEqualTo(state)
+				.jsonPath("slug").isEqualTo(slug)
+				.jsonPath("createdAt").isNotEmpty()
+				.jsonPath("updatedAt").isNotEmpty();
+	}
+
+	@Test
+	public void testCreatePlaceFailure() {
+		var name = "";
+		var state = "";
+		webTestClient
+				.post()
+				.uri("/places")
+				.bodyValue(
+						new PlaceRequest(name, state))
+				.exchange()
+				.expectStatus().isBadRequest();
+	}
+
+}
